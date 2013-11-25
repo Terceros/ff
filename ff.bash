af363ecad28a7d5d971958f8880758175acf54fd
Thomas Alcala <talcala@kaplan.edu>
2013-11-25 13:33:01 -0600

    init

---
+++ ff.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+path="$1"
+filepattern="$2"
+pattern="$3"
+
+function help {
+	printf "\nUse: $0 path file_pattern pattern\n\npath\t\t\tPath to search in\nfile_pattern\t\tA file name or pattern\npattern\t\t\tA pattern to search in the file(s)\n\ne.g.\t$0 . *.py create_course_shell\n"
+}
+
+if [[ -d "$path" ]]; then
+	if [[ "$pattern" == "" ]]; then
+		help
+		exit 1
+	else
+		find "$path" -name "$filepattern" | while read file; do
+			is_present=`grep "$pattern" "$file" | wc -l`
+			if [ $is_present -ge 1 ]; then
+				echo "$file"
+				grep -n "$pattern" "$file"
+			fi
+		done
+	fi
+else
+	help
+	exit 1
+fi
